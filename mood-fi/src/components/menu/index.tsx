69ae853b5678cf0addb3c80265594a8c245c6213
Kayky Dias <kayky.dias@brilliantmachine.com.br>
2022-11-22 18:01:40 -0300

    - lateral menu structure

--- mood-fi/src/components/menu/index.tsx
+++ mood-fi/src/components/menu/index.tsx
@@ -1,153 +1,67 @@
 import { useState } from "react";
-import { createStyles, Navbar, Group, Code } from "@mantine/core";
+import { Navbar, Group, Code, useMantineTheme, Box } from "@mantine/core";
 import { Link } from "react-router-dom";
-// import {
-//   IconBellRinging,
-//   IconFingerprint,
-//   IconKey,
-//   IconSettings,
-//   Icon2fa,
-//   IconDatabaseImport,
-//   IconReceipt2,
-//   IconSwitchHorizontal,
-//   IconLogout,
-// } from "@tabler/icons";
-// import { MantineLogo } from "@mantine/ds";
+import { MaterialIcon } from "../material-icon";
 
-const useStyles = createStyles((theme, _params, getRef) => {
-  const icon = getRef("icon");
-  return {
-    header: {
-      paddingBottom: theme.spacing.md,
-      marginBottom: theme.spacing.md * 1.5,
-      borderBottom: `1px solid ${
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[4]
-          : theme.colors.gray[2]
-      }`,
-    },
-
-    footer: {
-      paddingTop: theme.spacing.md,
-      marginTop: theme.spacing.md,
-      borderTop: `1px solid ${
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[4]
-          : theme.colors.gray[2]
-      }`,
-    },
-
-    link: {
-      ...theme.fn.focusStyles(),
-      display: "flex",
-      alignItems: "center",
-      textDecoration: "none",
-      fontSize: theme.fontSizes.sm,
-      color:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[1]
-          : theme.colors.gray[7],
-      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
-      borderRadius: theme.radius.sm,
-      fontWeight: 500,
-
-      "&:hover": {
-        backgroundColor:
-          theme.colorScheme === "dark"
-            ? theme.colors.dark[6]
-            : theme.colors.gray[0],
-        color: theme.colorScheme === "dark" ? theme.white : theme.black,
-
-        [`& .${icon}`]: {
-          color: theme.colorScheme === "dark" ? theme.white : theme.black,
-        },
-      },
-    },
-
-    linkIcon: {
-      ref: icon,
-      color:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[2]
-          : theme.colors.gray[6],
-      marginRight: theme.spacing.sm,
-    },
-
-    linkActive: {
-      "&, &:hover": {
-        backgroundColor: theme.fn.variant({
-          variant: "light",
-          color: theme.primaryColor,
-        }).background,
-        color: theme.fn.variant({ variant: "light", color: theme.primaryColor })
-          .color,
-        [`& .${icon}`]: {
-          color: theme.fn.variant({
-            variant: "light",
-            color: theme.primaryColor,
-          }).color,
-        },
-      },
-    },
-  };
-});
+import classes from "./index.module.scss";
+import { useMediaQuery } from "@mantine/hooks";
 
 const data = [
-  { link: "/", label: "Watch Live" },
-  { link: "/discover", label: "Discover" },
-  { link: "/references", label: "References" },
-  { link: "/settings", label: "Settings" },
+  {
+    link: "/",
+    icon: "online_prediction",
+    isFilled: false,
+    label: "Watch Live",
+  },
+  {
+    link: "/discover",
+    icon: "manage_search",
+    isFilled: false,
+    label: "Discover",
+  },
+  {
+    link: "/references",
+    icon: "temp_preferences_custom",
+    isFilled: false,
+    label: "References",
+  },
+  { link: "/settings", icon: "settings", isFilled: true, label: "Settings" },
 ];
 
 export function LateralMenu() {
-  const { classes, cx } = useStyles();
-  const [active, setActive] = useState("Billing");
+  const theme = useMantineTheme();
+
+  const minResolution = useMediaQuery("(max-width: 1000px)");
 
   const links = data.map((item) => (
-    <Link
-      className={cx(classes.link, {
-        [classes.linkActive]: item.label === active,
-      })}
-      to={item.link}
-      key={item.label}
-      onClick={(event) => {
-        setActive(item.label);
-      }}
-    >
-      {/* <item.icon className={classes.linkIcon} stroke={1.5} /> */}
-      <span>{item.label}</span>
+    <Link to={item.link} key={item.label} className={classes.menuLinks}>
+      <Group position={minResolution ? "center" : "left"}>
+        <MaterialIcon iconName={item.icon} size={20} filled={item.isFilled} />
+        {minResolution ? <></> : <span>{item.label}</span>}
+      </Group>
     </Link>
   ));
 
   return (
-    <Navbar height={700} width={{ sm: 300 }} p="md">
-      <Navbar.Section grow>
-        <Group className={classes.header} position="apart">
-          {/* <MantineLogo size={28} /> */}
-          <Code sx={{ fontWeight: 700 }}>v2.0.0</Code>
-        </Group>
-        {links}
-      </Navbar.Section>
-
-      <Navbar.Section className={classes.footer}>
-        <a
-          href="#"
-          className={classes.link}
-          onClick={(event) => event.preventDefault()}
-        >
-          {/* <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} /> */}
-          <span>Change account</span>
-        </a>
-
-        <a
-          href="#"
-          className={classes.link}
-          onClick={(event) => event.preventDefault()}
-        >
-          {/* <IconLogout className={classes.linkIcon} stroke={1.5} /> */}
-          <span>Logout</span>
-        </a>
-      </Navbar.Section>
-    </Navbar>
+    <div>
+      <Navbar
+        p="xs"
+        width={{ base: minResolution ? 100 : 300 }}
+        className={[classes.root, classes[theme.colorScheme]].join(" ")}
+      >
+        <Navbar.Section>
+          <Box className={classes.navBarHeader}>
+            <Group position="apart">
+              {/* <Logo colorScheme={colorScheme} /> */}
+              <Code className={classes.version}>v3.1.2</Code>
+            </Group>
+          </Box>
+        </Navbar.Section>
+        <Navbar.Section grow mt="md">
+          {links}
+        </Navbar.Section>
+        <Navbar.Section>{/* Footer with user */}</Navbar.Section>
+      </Navbar>
+    </div>
   );
 }
