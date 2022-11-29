a9c8e6770d63af25b0e04fa19c347d9137d85c05
Kayky Dias <kayky.dias@brilliantmachine.com.br>
2022-11-29 16:13:20 -0300

    - offline livestream database implementation by local storage

--- mood-fi/pages/discover/index.tsx
+++ mood-fi/pages/discover/index.tsx
@@ -15,67 +15,29 @@ import { getLivestreams } from "../../src/services";
 import { ILivestreams, ILivestreamsItems } from "../../src/models";
 import { LivestreamsCards } from "../../src/components";
 
-const CHANNELS_ID = {
-  livestreams: [
-    {
-      id: "UCSJ4gkVC6NrvII8umztf0Ow",
-    },
-    {
-      id: "UC2fVSthyWxWSjsiEAHPzriQ",
-    },
-    {
-      id: "UCncxHd8o_VhhHAJ7QqB5azg",
-    },
-    {
-      id: "UCxWNEhY-SNHRvF9Q2LWLK1g",
-    },
-    {
-      id: "UCWzZ5TIGoZ6o-KtbGCyhnhg",
-    },
-  ],
-};
-
 export const DiscoverPage = () => {
   const [livestreams, setLivestreams] = useState<ILivestreamsItems[]>([]);
 
   useEffect(() => {
-    // CHANNELS_ID.livestreams.map((id) => {
-    //   load(id.id);
-    // });
+    load();
   }, []);
 
-  async function load(id: string) {
-    let livestreamsArray: ILivestreams[] = [];
-
-    livestreamsArray.push(await getLivestreams(id));
-    return getLivestreamsItems(livestreamsArray.map((l) => l.items));
+  async function load() {
+    if (localStorage.getItem("Livestreams")) {
+      setLivestreams(JSON.parse(localStorage.getItem("Livestreams")!));
+    }
   }
-
-  function getLivestreamsItems(items: ILivestreamsItems[][]) {
-    let itemsArray: ILivestreamsItems[] = [];
-    items.map((i) =>
-      i.map((ii) => {
-        return itemsArray.push(ii);
-      })
-    );
-
-    setLivestreams(itemsArray);
-  }
-
-  // console.log(livestreams.map((i) => i.snippet.liveBroadcastContent));
-  // console.log(CHANNELS_ID.livestreams);
-  // console.log(livestreams);
 
   return (
     <>
       <div>
         <h1>descobrium</h1>
       </div>
-      {/* <Group>
+      <Group>
         {livestreams.map((l) => {
           return <LivestreamsCards key={l.id.videoId} videoId={l.id.videoId} />;
         })}
-      </Group> */}
+      </Group>
     </>
   );
 };
