b5409bef12e836218f45a1656c582ef0aff86785
Kayky Dias <kaykydiasw@outlook.com.br>
2022-12-02 18:31:14 -0300

    - nav bar footer styling & responsivity

--- mood-fi/src/components/menu-components/navbar-footer/index.tsx
+++ mood-fi/src/components/menu-components/navbar-footer/index.tsx
@@ -1,7 +1,7 @@
 import { Box, Group, Stack, Text, useMantineTheme } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
-import { useState } from "react";
-import { channelsDatabase } from "../../../database";
+import { useEffect, useState } from "react";
+import { channelsProfilePictureDatabase } from "../../../database";
 import { useLivestream } from "../../../hooks";
 import classes from "./index.module.scss";
 
@@ -12,6 +12,18 @@ export function NavBarFooter() {
 
   const { selectedLivestream } = useLivestream();
 
+  const [channelUrl, setChannelUrl] = useState("");
+
+  useEffect(() => {
+    setChannelUrl(
+      channelsProfilePictureDatabase
+        .filter(
+          (channel) => channel.id === selectedLivestream.snippet.channelId
+        )
+        .map((ch) => ch.url)[0]
+    );
+  }, [selectedLivestream]);
+
   return (
     <Stack className={[classes.root, classes[theme.colorScheme]].join(" ")}>
       <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
@@ -21,67 +33,56 @@ export function NavBarFooter() {
               ? "../../../../public/Mood-Fi mini logo.svg"
               : "../../../../public/Mood-Fi logo.svg"
           }
-          width={minWidthResolution || minHeightResolution ? 70 : 80}
-          height={minWidthResolution || minHeightResolution ? 60 : 70}
+          width={minWidthResolution || minHeightResolution ? 50 : 80}
+          height={minWidthResolution || minHeightResolution ? 40 : 70}
           alt=""
         />
       </div>
       <Stack className={classes.selectedLivestreamContainer}>
-        {/* <div
-          style={{
-            width: "50px",
-            height: "50px",
-            borderRadius: "20px",
-          }}
-        >
-          <img
-            className={classes.selectedLivestreamImage}
-            width="100%"
-            height="100%"
-            src={
-              channelsDatabase.filter(
-                (channel) =>
-                  channel.snippet.channelId ===
-                  selectedLivestream.snippet.channelId
-              )[0].snippet.thumbnails.high.url
-            }
-            alt=""
-          />
-        </div> */}
         {minWidthResolution || minHeightResolution ? (
           <div
             style={{
-              width: "50px",
-              height: "50px",
-              borderRadius: "20px",
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+
+              marginBottom: "30px",
             }}
           >
-            <img
-              className={classes.selectedLivestreamImage}
-              width="100%"
-              height="100%"
-              src={
-                channelsDatabase.filter(
-                  (channel) =>
-                    channel.snippet.channelId ===
-                    selectedLivestream.snippet.channelId
-                )[0].snippet.thumbnails.high.url
-              }
-              alt=""
-            />
+            <div
+              style={{
+                width: "50px",
+                height: "50px",
+
+                overflow: "hidden",
+
+                borderRadius: "50px",
+              }}
+            >
+              <img
+                className={classes.selectedLivestreamImage}
+                width="50px"
+                height="50px"
+                src={channelUrl}
+                alt=""
+              />
+            </div>
           </div>
         ) : (
-          <img
-            className={classes.selectedLivestreamImage}
-            height="250px"
-            src={selectedLivestream.snippet.thumbnails.medium.url}
-            alt=""
-          />
+          <>
+            <img
+              className={classes.selectedLivestreamImage}
+              height="250px"
+              src={selectedLivestream.snippet.thumbnails.medium.url}
+              alt=""
+            />
+            <Box className={classes.gradient} />
+            <Text className={classes.selectedLivestreamTitle}>
+              {selectedLivestream.snippet.channelTitle}
+            </Text>
+          </>
         )}
-        <Box className={classes.gradient} />
-        <Text className={classes.selectedLivestreamTitle}>
-          {selectedLivestream.snippet.channelTitle}
-        </Text>
       </Stack>
     </Stack>
   );
