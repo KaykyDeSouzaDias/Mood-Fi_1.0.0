2d8919f78af5585466ba4d675f6f23e698142ddf
Kayky Dias <kaykydiasw@outlook.com.br>
2022-12-10 19:16:31 -0300

    - channels social medias database information

--- mood-fi/src/database/channels.ts
+++ mood-fi/src/database/channels.ts
@@ -13,6 +13,18 @@ export const channelsDatabase = [
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@MrMoMoMusic",
       },
+      {
+        mediaName: "discord",
+        mediaLink: "https://discord.com/invite/6SGnSpCkhy",
+      },
+      {
+        mediaName: "vk",
+        mediaLink: "https://vk.com/lofi.trapanese",
+      },
+      {
+        mediaName: "twitter",
+        mediaLink: "https://twitter.com/mrmomolofi",
+      },
     ],
   },
   {
@@ -26,6 +38,18 @@ export const channelsDatabase = [
       {
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@LofiGirl",
+      },
+      {
+        mediaName: "store",
+        mediaLink: "https://lofigirlshop.com/",
+      },
+      {
+        mediaName: "spotify",
+        mediaLink: "https://open.spotify.com/user/chilledcow",
+      },
+      {
+        mediaName: "instagram",
+        mediaLink: "https://www.instagram.com/lofigirl/",
       },
     ],
   },
@@ -41,6 +65,18 @@ export const channelsDatabase = [
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@ChilledEmpire",
       },
+      {
+        mediaName: "spotify",
+        mediaLink: "https://open.spotify.com/user/1zrtlf4hnwbntox304gl42zvb",
+      },
+      {
+        mediaName: "discord",
+        mediaLink: "https://discord.com/invite/34SfKKWJsa",
+      },
+      {
+        mediaName: "instagram",
+        mediaLink: "https://www.instagram.com/chilledempire/",
+      },
     ],
   },
   {
@@ -54,6 +90,18 @@ export const channelsDatabase = [
       {
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@nourish",
+      },
+      {
+        mediaName: "website",
+        mediaLink: "https://www.nourish.live/",
+      },
+      {
+        mediaName: "instagram",
+        mediaLink: "https://www.instagram.com/nourishmusic/",
+      },
+      {
+        mediaName: "spotify",
+        mediaLink: "https://open.spotify.com/user/nourish.",
       },
     ],
   },
@@ -69,6 +117,18 @@ export const channelsDatabase = [
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@ChillwithTaiki",
       },
+      {
+        mediaName: "store",
+        mediaLink: "https://taiki.shop/",
+      },
+      {
+        mediaName: "instagram",
+        mediaLink: "https://www.instagram.com/chillwithtaiki/",
+      },
+      {
+        mediaName: "playlist",
+        mediaLink: "https://fanlink.to/dtNC",
+      },
     ],
   },
   {
@@ -82,6 +142,18 @@ export const channelsDatabase = [
       {
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@DreamhopMusic",
+      },
+      {
+        mediaName: "bandcamp",
+        mediaLink: "https://dreamhop.bandcamp.com/",
+      },
+      {
+        mediaName: "instagram",
+        mediaLink: "https://www.instagram.com/dreamhopp/",
+      },
+      {
+        mediaName: "spotify",
+        mediaLink: "https://open.spotify.com/user/91jfqzlv0htrqrvvc60138qma",
       },
     ],
   },
@@ -97,6 +169,18 @@ export const channelsDatabase = [
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@ChillhopMusic",
       },
+      {
+        mediaName: "website",
+        mediaLink: "https://chillhop.com/",
+      },
+      {
+        mediaName: "appleMusic",
+        mediaLink: "https://music.apple.com/us/label/chillhop-music/1548562904",
+      },
+      {
+        mediaName: "spotify",
+        mediaLink: "https://open.spotify.com/artist/7tD7DSEvtLAc94x6oH7NLJ",
+      },
     ],
   },
   {
@@ -110,6 +194,18 @@ export const channelsDatabase = [
       {
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@CollegeMusic",
+      },
+      {
+        mediaName: "website",
+        mediaLink: "https://collegemusic.co.uk/",
+      },
+      {
+        mediaName: "instagram",
+        mediaLink: "https://www.instagram.com/collegemusic/",
+      },
+      {
+        mediaName: "spotify",
+        mediaLink: "https://open.spotify.com/user/college.music",
       },
     ],
   },
@@ -125,6 +221,18 @@ export const channelsDatabase = [
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@lofigeek",
       },
+      {
+        mediaName: "discord",
+        mediaLink: "https://discord.com/invite/fs7qxvVAnN",
+      },
+      {
+        mediaName: "instagram",
+        mediaLink: "https://www.instagram.com/lofigeek/",
+      },
+      {
+        mediaName: "spotify",
+        mediaLink: "https://open.spotify.com/artist/1hqeRW1EQZ1flf02acnGVU",
+      },
     ],
   },
   {
@@ -138,6 +246,18 @@ export const channelsDatabase = [
       {
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@steezyasfvck",
+      },
+      {
+        mediaName: "website",
+        mediaLink: "https://www.stzzzy.com/",
+      },
+      {
+        mediaName: "instagram",
+        mediaLink: "https://www.instagram.com/stzzyasfvck/",
+      },
+      {
+        mediaName: "spotify",
+        mediaLink: "https://open.spotify.com/user/stzzzy",
       },
     ],
   },
@@ -153,6 +273,10 @@ export const channelsDatabase = [
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@CoffeeShopVibes",
       },
+      {
+        mediaName: "spotify",
+        mediaLink: "https://open.spotify.com/artist/0N1It883FKaZoTydOuWVyW",
+      },
     ],
   },
   {
@@ -167,6 +291,18 @@ export const channelsDatabase = [
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@CodePioneers",
       },
+      {
+        mediaName: "store",
+        mediaLink: "https://codepioneers.myspreadshop.com/",
+      },
+      {
+        mediaName: "discord",
+        mediaLink: "https://discord.com/invite/8ZT4b4G75R",
+      },
+      {
+        mediaName: "spotify",
+        mediaLink: "https://open.spotify.com/user/12140951657",
+      },
     ],
   },
   {
@@ -180,6 +316,18 @@ export const channelsDatabase = [
       {
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@TokyoTones",
+      },
+      {
+        mediaName: "website",
+        mediaLink: "https://www.tokyotones.com/",
+      },
+      {
+        mediaName: "instagram",
+        mediaLink: "https://www.instagram.com/benjamin_hung/",
+      },
+      {
+        mediaName: "patreon",
+        mediaLink: "https://www.patreon.com/tokyotones",
       },
     ],
   },
@@ -197,6 +345,20 @@ export const channelsDatabase = [
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@RelaxationMeditationMusic",
       },
+      {
+        mediaName: "amazon",
+        mediaLink:
+          "https://music.amazon.com/artists/B08P2KM7H6/blue-wave-studio",
+      },
+      {
+        mediaName: "apple",
+        mediaLink:
+          "https://music.apple.com/ca/artist/blue-wave-studio/1541459413",
+      },
+      {
+        mediaName: "spotify",
+        mediaLink: "https://open.spotify.com/artist/3D3Xe3oFYlF07jveNbcMtn",
+      },
     ],
   },
   {
@@ -210,6 +372,18 @@ export const channelsDatabase = [
       {
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@HomeworkRadio",
+      },
+      {
+        mediaName: "coffee",
+        mediaLink: "https://ko-fi.com/homeworkradio",
+      },
+      {
+        mediaName: "discord",
+        mediaLink: "https://discord.com/invite/s5sY6HT",
+      },
+      {
+        mediaName: "spotify",
+        mediaLink: "https://open.spotify.com/user/qpjtj9amkbzwlmt6ora8qmoih",
       },
     ],
   },
@@ -225,6 +399,18 @@ export const channelsDatabase = [
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@MusicLabChill",
       },
+      {
+        mediaName: "website",
+        mediaLink: "https://musiclab.cloud/",
+      },
+      {
+        mediaName: "patreon",
+        mediaLink: "https://www.patreon.com/music_lab",
+      },
+      {
+        mediaName: "spotify",
+        mediaLink: "https://open.spotify.com/user/9aqvyj9581kg3j582a4h3vfpa",
+      },
     ],
   },
   {
@@ -238,6 +424,18 @@ export const channelsDatabase = [
       {
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@LofiFruits",
+      },
+      {
+        mediaName: "playlist",
+        mediaLink: "https://lnk.to/lofifruitsmusic",
+      },
+      {
+        mediaName: "instagram",
+        mediaLink: "https://www.instagram.com/lofifruitsmusic/",
+      },
+      {
+        mediaName: " spotify",
+        mediaLink: "https://open.spotify.com/user/a902190",
       },
     ],
   },
@@ -253,6 +451,18 @@ export const channelsDatabase = [
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@thebootlegboy2",
       },
+      {
+        mediaName: "store",
+        mediaLink: "thebootlegboy.com",
+      },
+      {
+        mediaName: "instagram",
+        mediaLink: "https://www.instagram.com/thebootlegboy/",
+      },
+      {
+        mediaName: "spotify",
+        mediaLink: "https://open.spotify.com/user/thebootlegboy",
+      },
     ],
   },
   {
@@ -266,6 +476,18 @@ export const channelsDatabase = [
       {
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@SACRABEATS",
+      },
+      {
+        mediaName: "playlist",
+        mediaLink: "https://lnk.to/TokyoLosTTracks",
+      },
+      {
+        mediaName: "instagram",
+        mediaLink: "https://www.instagram.com/sakurachillbeats/",
+      },
+      {
+        mediaName: "discord",
+        mediaLink: "https://discord.com/invite/fHkhEgc",
       },
     ],
   },
@@ -281,6 +503,18 @@ export const channelsDatabase = [
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@InYourChill",
       },
+      {
+        mediaName: "website",
+        mediaLink: "https://inyourchill.com/",
+      },
+      {
+        mediaName: "instagram",
+        mediaLink: "https://www.instagram.com/InYourChillMusic/",
+      },
+      {
+        mediaName: "spotify",
+        mediaLink: "https://open.spotify.com/user/ersxxojg18hnzmir1jweb1oav",
+      },
     ],
   },
   {
@@ -294,6 +528,18 @@ export const channelsDatabase = [
       {
         mediaName: "youtube",
         mediaLink: "https://www.youtube.com/@nostalgicmusic",
+      },
+      {
+        mediaName: "store",
+        mediaLink: "https://nostalgic-14.creator-spring.com/",
+      },
+      {
+        mediaName: "discord",
+        mediaLink: "https://discord.com/invite/qCq8z7ufJP",
+      },
+      {
+        mediaName: "playlist",
+        mediaLink: "https://ffm.to/anifi",
       },
     ],
   },
