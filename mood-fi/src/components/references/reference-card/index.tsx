0a4ceebc37f4971f7b9949c7c17f43b7281f4248
Kayky Dias <kaykydiasw@outlook.com.br>
2022-12-12 14:15:57 -0300

    - reference card styling

--- mood-fi/src/components/references/reference-card/index.tsx
+++ mood-fi/src/components/references/reference-card/index.tsx
@@ -6,66 +6,79 @@ import {
   Group,
   Button,
   ActionIcon,
+  useMantineTheme,
+  Tooltip,
 } from "@mantine/core";
-import { IconBrandYoutube } from "@tabler/icons";
-import { IChannel } from "../../../models";
+import {
+  IconBrandAmazon,
+  IconBrandApple,
+  IconBrandBandcamp,
+  IconBrandDiscord,
+  IconBrandInstagram,
+  IconBrandPatreon,
+  IconBrandSpotify,
+  IconBrandTwitter,
+  IconBrandVk,
+  IconBrandYoutube,
+  IconBuildingStore,
+  IconCoffee,
+  IconDeviceDesktop,
+  IconPlaylist,
+} from "@tabler/icons";
+import { IChannel, IChannelSocialMedias } from "../../../models";
 
-const stat = {
-  image:
-    "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80",
-  avatar:
-    "https://images.unsplash.com/photo-1623582854588-d60de57fa33f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80",
-  name: "Bill Headbanger",
-  job: "Fullstack engineer",
-  stats: [
-    {
-      value: "34K",
-      label: "Followers",
-    },
-    {
-      value: "187",
-      label: "Follows",
-    },
-    {
-      value: "1.6K",
-      label: "Posts",
-    },
-  ],
-};
-
-const useStyles = createStyles((theme) => ({
-  card: {
-    backgroundColor:
-      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
-  },
-
-  // avatar: {
-  //   border: `2px solid ${
-  //     theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white
-  //   }`,
-  // },
-}));
+import classes from "./index.module.scss";
 
 interface ReferenceCardProps {
   actualChannel: IChannel;
 }
 
 export function ReferenceCard({ actualChannel }: ReferenceCardProps) {
-  const { classes, theme } = useStyles();
+  const theme = useMantineTheme();
 
-  const items = stat.stats.map((stat) => (
-    <div key={stat.label}>
-      <Text align="center" size="lg" weight={500}>
-        {stat.value}
-      </Text>
-      <Text align="center" size="sm" color="dimmed">
-        {stat.label}
-      </Text>
-    </div>
-  ));
+  function getMediaIcon(media: IChannelSocialMedias, iconSize: number) {
+    switch (media.mediaName) {
+      case "Youtube":
+        return <IconBrandYoutube size={iconSize} />;
+      case "Discord":
+        return <IconBrandDiscord size={iconSize} />;
+      case "Spotify":
+        return <IconBrandSpotify size={iconSize} />;
+      case "Apple music":
+        return <IconBrandApple size={iconSize} />;
+      case "Official store":
+        return <IconBuildingStore size={iconSize} />;
+      case "Official playlist":
+        return <IconPlaylist size={iconSize} />;
+      case "Instagram":
+        return <IconBrandInstagram size={iconSize} />;
+      case "Official website":
+        return <IconDeviceDesktop size={iconSize} />;
+      case "Patreon":
+        return <IconBrandPatreon size={iconSize} />;
+      case "Coffee":
+        return <IconCoffee size={iconSize} />;
+      case "Amazon music":
+        return <IconBrandAmazon size={iconSize} />;
+      case "Bandcamp":
+        return <IconBrandBandcamp size={iconSize} />;
+      case "Twitter":
+        return <IconBrandTwitter size={iconSize} />;
+      case "VK":
+        return <IconBrandVk size={iconSize} />;
+
+      default:
+        break;
+    }
+  }
 
   return (
-    <Card withBorder p="xl" radius="md" className={classes.card}>
+    <Card
+      withBorder={false}
+      p="xl"
+      radius="md"
+      className={[classes.root, classes[theme.colorScheme]].join(" ")}
+    >
       <Card.Section
         sx={{
           backgroundImage: `url(${actualChannel.image.channelBanner})`,
@@ -80,30 +93,24 @@ export function ReferenceCard({ actualChannel }: ReferenceCardProps) {
         radius={80}
         mx="auto"
         mt={-30}
-        // className={classes.avatar}
       />
       <Text align="center" size="lg" weight={500} mt="sm">
         {actualChannel.channelName}
       </Text>
       <Group mt="md" position="center" spacing={30}>
         {actualChannel.socialMedias.map((media) => {
-          switch (media.mediaName) {
-            case "youtube":
-              return (
-                <ActionIcon
-                  size="lg"
-                  component="a"
-                  href={media.mediaLink}
-                  target="_blank"
-                >
-                  <IconBrandYoutube size={18} />
-                </ActionIcon>
-              );
-              break;
-
-            default:
-              break;
-          }
+          return (
+            <Tooltip label={media.mediaName}>
+              <ActionIcon
+                size="lg"
+                component="a"
+                href={media.mediaLink}
+                target="_blank"
+              >
+                {getMediaIcon(media, 18)}
+              </ActionIcon>
+            </Tooltip>
+          );
         })}
       </Group>
     </Card>
