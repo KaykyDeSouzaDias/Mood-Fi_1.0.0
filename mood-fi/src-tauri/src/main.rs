a0a90bd9a668b9840827554517574a34b83d5fca
Kayky Dias <kayky.dias@brilliantmachine.com.br>
2023-01-25 15:58:21 -0300

    - splashscreen reconstruction

--- mood-fi/src-tauri/src/main.rs
+++ mood-fi/src-tauri/src/main.rs
@@ -5,19 +5,19 @@ use tauri::Manager;
     windows_subsystem = "windows"
 )]
 
-#[tauri::command]
-async fn close_splashscreen(window: tauri::Window) {
-  // Close splashscreen
-  if let Some(splashscreen) = window.get_window("splashscreen") {
-    splashscreen.close().unwrap();
-  }
-  // Show main window
-  window.get_window("main").unwrap().show().unwrap();
-}
-
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![close_splashscreen])
+        .setup(|app| {
+          let splashscreen_window = app.get_window("splashscreen").unwrap();
+          let main_window = app.get_window("main").unwrap();
+          tauri::async_runtime::spawn(async move {
+            std::thread::sleep(std::time::Duration::from_secs(5));
+
+            splashscreen_window.close().unwrap();
+            main_window.show().unwrap();
+          });
+          Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
